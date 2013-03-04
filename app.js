c1d0c4a70ebb104c2c5f448d4c197fa0c284d4a1
Rajiv Kilaparti <rajivk085@gmail.com>
2013-03-05 00:40:37 +0530

    Refactor Code

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 express = require("express");
-app = express();
 connect = require("connect");
 bytes = require('bytes');
 ejs = require('ejs');
 utils = require("./utils/utils");
 log = require("./utils/logger");
 config = require('./config/config');
+creek = require('./controls/creek');
 passport = require('passport');
 LocalStrategy = require('passport-local').Strategy;
 
-require('./settings/appSettings')();
+app = express();
 this.log = new log.Logger();
 
-server = app.listen(app.settings.PORT);
+require('./settings/appSettings')();
 require('./routes/routes')(this.log);
+require('./controls/passport')
+
+server = app.listen(app.settings.PORT);
+this.log.info('Server started at PORT: ' + app.settings.PORT);
