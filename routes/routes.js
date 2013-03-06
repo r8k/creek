8f5ceb8af54b10b761358b58b444026106c81cd0
Rajiv Kilaparti <rajivk085@gmail.com>
2013-03-06 08:18:04 +0530

    routes docs

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,19 @@
+// # routes.js
+// routes.js defines all our available routes and what we do with them.
 app.get('/', ensureAuthenticated, creek.home);
 app.get('/item/:id', ensureAuthenticated, creek.fetch);
 app.get('/allitems', ensureAuthenticated, creek.fetchall);
 app.get('/items', ensureAuthenticated, creek.create);
 app.post('/item', ensureAuthenticated, creek.create);
+
+// And as said earlier, we do use passport for authentication,
+// and use the LocalStrategy to work for us.
 app.post('/login', passport.authenticate('local', {
     successRedirect: '/items',
     failureRedirect: '/'
 }));
 
+// For all unnecessary routes, re-route them to our index page.
 app.get('*', function (req, res) {
     res.redirect('/');
 });
