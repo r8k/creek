f2493fbbe374cdc38de98ba6c4196a0ddc3cc2ef
Rajiv Kilaparti <rajivk085@gmail.com>
2013-03-06 07:51:04 +0530

    Updated docs

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
+// # config.js
+// config.js is the central place for us to have all the required
+// configurations loaded. Here we add every parameter that can & should be configured.
+// Right now, as you can see, we are having only usergridClientOptions.
 exports.usergridClientOptions = usergridClientOptions = {
     orgName: 'r8k',
     appName: 'sandbox'
