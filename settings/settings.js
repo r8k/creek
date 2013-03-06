7c962f0ece1785cc34351eaa8a308fae1d82ca97
Rajiv Kilaparti <rajivk085@gmail.com>
2013-03-06 07:55:08 +0530

    settings docs

--- settings/settings.js
+++ settings/settings.js
@@ -1,9 +1,15 @@
+// # settings.js
+// settings.js is used for the express related app settings.
+// We have everything related to express setting loaded here, from
+// cache settings, to logger format, connect middleware settings,
+// passport settings, cookieParser related ones, errorHandlers,
+// view rendering engines, public root path, session secrets etc and so on.
 var oneYear = 31557600000;
-express.logger.format('customLog', utils.customLog);
+express.logger.format('expresslog', utils.expresslog);
 app.use(connect.compress());
 app.use(express.bodyParser());
 app.use(express.errorHandler());
-app.use(express.logger('express-log'));
+app.use(express.logger('expresslog'));
 app.engine('html', ejs.renderFile);
 app.set('views', __dirname + '/../views');
 app.use(express.cookieParser());
