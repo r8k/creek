dbde50d9e1ff6837c04d4330424842e6e0522d42
Rajiv Kilaparti <rajivk085@gmail.com>
2013-03-08 22:26:19 +0530

    remove dead code

--- utils/utils.js
+++ utils/utils.js
@@ -1,34 +1,3 @@
-var bytes = require('bytes');
-
-exports.dateFormat = dateFormat = function (date, fstr, utc) {
-	/*
-	 * Usage: var dateNow = this.dateFormat (new Date (),
-	 * 							"%Y-%m-%d %H:%M:%S", false);
-	 */
-	utc = utc ? 'getUTC' : 'get';
-	return fstr.replace (/%[YmdHMS]/g, function (m) {
-		switch (m) {
-			case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
-			case '%m': m = 1 + date[utc + 'Month'] (); break;
-			case '%d': m = date[utc + 'Date'] (); break;
-			case '%H': m = date[utc + 'Hours'] (); break;
-			case '%M': m = date[utc + 'Minutes'] (); break;
-			case '%S': m = date[utc + 'Seconds'] (); break;
-			default: return m.slice (1); // unknown code, remove %
-		}
-		/**
-		 * add leading zero if required
-		 */
-		return ('0' + m).slice (-2);
-	});
-};
-
-exports.formatLogMsg = formatLogMsg = function (requestBody) {
-	var dateNow = '[' + (new Date()).toLocaleString().slice(0,24) + '] ';
-	requestBody.logMsg = dateNow + ' ' + requestBody.logMsg;
-	return requestBody;
-};
-
 exports.expresslog = expresslog = function(tokens, req, res){
   var status = res.statusCode
   , len = parseInt(res.getHeader('Content-Length'), 10)
